ef9228fdd0f73040cf0eb4e7cea7b89588b4980f
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-07-28 19:29:05 +0600

    added jsx and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import data from "../data/data.json";
 
 const App = () => {
-  return <div>APP</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="step-1 active">1</div>
+        <div className="step-2">2</div>
+        <div className="step-3">3</div>
+      </div>
+
+      <p className="message">{data[0]}</p>
+
+      <div className="buttons">
+        <button className="previous">Previous</button>
+        <button className="next">Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
